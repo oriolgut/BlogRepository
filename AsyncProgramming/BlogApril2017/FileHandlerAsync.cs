9145ada8a3e80343f9ee235c41eaa4b0a6dbdfe6
oriolgut <oriol.gut@noser.com>
2017-04-27 08:21:09 +0200

    added async file handler with read function

---
+++ AsyncProgramming/BlogApril2017/FileHandlerAsync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApril2017
+{
+    public class FileHandlerAsync
+    {
+        private const int DEFAULT_BUFFER_SIZE = 4096;
+
+        public static async Task<string> ReadAllLinesAsync(string @path)
+        {
+            if(!File.Exists(path)) throw new ArgumentException($"The File {path} doesn't exists");
+            var lines = new List<string>();
+
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.Read, DEFAULT_BUFFER_SIZE, FileOptions.Asynchronous | FileOptions.SequentialScan))
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+                //string line;
+                //while ((line = await reader.ReadLineAsync()) != null)
+                //{
+                //    lines.Add(line);
+                //}
+            }
+            //return lines;           
+        }
+    }
+}
